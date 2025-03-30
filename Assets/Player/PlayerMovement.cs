39cd3ffc651015a9d9ddf7716721bb900f632706
Charlie Wang <charlie.legobrick@gmail.com>
2025-03-30 01:16:18 -0400

    PlayerMovement Fix

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -1,38 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Values")]
     public float moveSpeed;
-
     public float acceleration;
     public float deceleration;
-
     public float dashSpeed;
     public float dashDistance;
     public float dashCooldown;
-
     public float collisionMoveDistance;
     public LayerMask layerMask;
+    private CharacterController cc; 
 
     [Header("References")]
     public Animator animator;
     public Rigidbody rb;
-
     public MeshTrail meshTrail;
-
     public SpellManager spellManager;
 
     [Header("Info Variables")]
     public Vector3 moveDir = Vector3.zero;
-
+    private Vector3 move;
     public bool canDash;
-
     public bool isCastingSpell;
-
     private float currSpeed;
+
+    public Vector3 velocity;
+    public float groundCheckDistance;
+    public bool isGrounded; 
+    public float gravityForce;
+
 
     // Start is called before the first frame update
     void Start()
@@ -40,9 +42,10 @@ public class PlayerMovement : MonoBehaviour
         canDash = true;
 
         spellManager.playerRB = rb;
+        cc = this.GetComponent<CharacterController>();
     }
 
-    private void Update()
+    void Update()
     {
         float verticalInput = Input.GetAxisRaw("Vertical");
         float sidewaysInput = Input.GetAxisRaw("Horizontal");
@@ -74,13 +77,19 @@ public class PlayerMovement : MonoBehaviour
 
         currSpeed = Mathf.Clamp(currSpeed, 0f, moveSpeed);
 
-        if (!Physics.Raycast(transform.position, moveDir, collisionMoveDistance, layerMask))
-        {
-            transform.position += moveDir * currSpeed * Time.deltaTime;
-        }
+        // if (!Physics.Raycast(transform.position, moveDir, collisionMoveDistance, layerMask))
+        // {
+        //     transform.position += moveDir * currSpeed * Time.deltaTime;
+        // }
+        ApplyGravity();
+        Vector3 dir = moveDir * currSpeed * Time.deltaTime;
+        move = new Vector3(dir.x, 0, dir.z);
+        cc.Move(move);
+        cc.Move(velocity);
+
 
         // Dashing
-        if (Input.GetButtonDown("Dash") && canDash)
+        if ((Input.GetButtonDown("Dash") || Input.GetKeyDown(KeyCode.LeftShift)) && canDash)
         {
             Dash();
         }
@@ -88,8 +97,8 @@ public class PlayerMovement : MonoBehaviour
 
     void Dash()
     {
-        Vector3 originalPos = rb.position;
-        Vector3 target = rb.position + dashDistance * moveDir;
+        Vector3 originalPos = transform.position;
+        Vector3 target = transform.position + dashDistance * moveDir;
 
         // target dash position
 
@@ -100,14 +109,24 @@ public class PlayerMovement : MonoBehaviour
         StartCoroutine(DashCooldown());
     }
 
+    void ApplyGravity()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, layerMask);
+        if (isGrounded) {
+            velocity = new Vector3(0f, -0.1f, 0f);
+        } else {
+            velocity.y -= gravityForce * Time.deltaTime; // Apply gravity
+        }
+        cc.Move(velocity * Time.deltaTime);
+    }
+
     private IEnumerator DashMovement(Vector3 start, Vector3 target)  // s is start, t is target
     {
         Vector3 dir = (target - start).normalized;
 
         while (Vector3.Distance(transform.position, start) < dashDistance)
         {
-            transform.position += dir * dashSpeed * Time.deltaTime;
-
+            cc.Move(dir * dashSpeed * Time.deltaTime);
             yield return new WaitForEndOfFrame();
         }
     }
@@ -118,4 +137,5 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
     }
+
 }
